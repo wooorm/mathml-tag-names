8584dfa5cbecfcdb35b6edaf3f2bb05dd23cb6ba
Titus Wormer <tituswormer@gmail.com>
2017-06-25 20:46:59 +0200

    Refactor build script

--- build.js
+++ build.js
@@ -1,77 +1,19 @@
 'use strict';
 
-/* Dependencies. */
 var fs = require('fs');
-var jsdom = require('jsdom');
+var https = require('https');
+var concat = require('concat-stream');
 var bail = require('bail');
+var unified = require('unified');
+var html = require('rehype-parse');
+var selectAll = require('hast-util-select').selectAll;
+var toString = require('hast-util-to-string');
 var list = require('./');
+
+var proc = unified().use(html);
 
 var count = 0;
 
-/* Crawl MathMl 1.0. */
-jsdom.env('https://www.w3.org/TR/1998/REC-MathML-19980407/appendixF.html', function (err, window) {
-  var nodes;
-
-  bail(err);
-
-  nodes = [].slice.call(window.document.querySelectorAll('ul ul ul ul a'));
-
-  nodes.forEach(function (node) {
-    var data = node.childNodes[0].textContent;
-
-    data = data.slice(data.indexOf('<') + 1, data.indexOf('>'));
-
-    if (data.charAt(data.length - 1) === '/') {
-      data = data.slice(0, -1);
-    }
-
-    if (list.indexOf(data) === -1) {
-      list.push(data);
-    }
-  });
-
-  done();
-});
-
-/* Crawl MathMl 2.0. */
-jsdom.env('https://www.w3.org/TR/MathML2/appendixl.html', function (err, window) {
-  var nodes;
-
-  bail(err);
-
-  nodes = [].slice.call(window.document.querySelectorAll('.div1 .div2:first-child dl dt'));
-
-  nodes.forEach(function (node) {
-    var data = node.childNodes[0].textContent;
-
-    if (list.indexOf(data) === -1) {
-      list.push(data);
-    }
-  });
-
-  done();
-});
-
-/* Crawl MathMl 3.0. */
-jsdom.env('https://www.w3.org/TR/MathML3/appendixi.html', function (err, window) {
-  var nodes;
-
-  bail(err);
-
-  nodes = [].slice.call(window.document.querySelectorAll('.div1 .div2:first-child dl dt'));
-
-  nodes.forEach(function (node) {
-    var data = node.childNodes[0].textContent;
-
-    if (list.indexOf(data) === -1) {
-      list.push(data);
-    }
-  });
-
-  done();
-});
-
-/* Write. */
 function done() {
   count++;
 
@@ -79,3 +21,66 @@ function done() {
     fs.writeFile('index.json', JSON.stringify(list.sort(), 0, 2) + '\n', bail);
   }
 }
+
+/* Crawl MathMl 1.0. */
+https.get('https://www.w3.org/TR/1998/REC-MathML-19980407/appendixF.html', function (res) {
+  res.pipe(concat(onconcat)).on('error', bail);
+
+  function onconcat(buf) {
+    selectAll('ul ul ul ul a', proc.parse(buf)).forEach(each);
+
+    done();
+
+    function each(node) {
+      var data = toString(node.children[0]);
+
+      data = data.slice(data.indexOf('<') + 1, data.indexOf('>'));
+
+      if (data.charAt(data.length - 1) === '/') {
+        data = data.slice(0, -1);
+      }
+
+      if (list.indexOf(data) === -1) {
+        list.push(data);
+      }
+    }
+  }
+});
+
+/* Crawl MathMl 2.0. */
+https.get('https://www.w3.org/TR/MathML2/appendixl.html', function (res) {
+  res.pipe(concat(onconcat)).on('error', bail);
+
+  function onconcat(buf) {
+    selectAll('.div1 .div2:first-child dl dt', proc.parse(buf)).forEach(each);
+
+    done();
+
+    function each(node) {
+      var data = toString(node);
+
+      if (list.indexOf(data) === -1) {
+        list.push(data);
+      }
+    }
+  }
+});
+
+/* Crawl MathMl 3.0. */
+https.get('https://www.w3.org/TR/MathML3/appendixi.html', function (res) {
+  res.pipe(concat(onconcat)).on('error', bail);
+
+  function onconcat(buf) {
+    selectAll('.div1 .div2:first-child dl dt', proc.parse(buf)).forEach(each);
+
+    done();
+
+    function each(node) {
+      var data = toString(node);
+
+      if (list.indexOf(data) === -1) {
+        list.push(data);
+      }
+    }
+  }
+});
